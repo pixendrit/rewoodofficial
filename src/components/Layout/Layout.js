2e2b913ed7881713c89f5aff7a575dcb5d31f909
pixendrit <pixendrit@outlook.com>
2021-03-24 11:54:49 +0100

    title final

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -148,7 +148,7 @@ const Layout = ({ children, pageContext }) => {
         <div data-theme-mode-panel-active data-theme="light">
           <GlobalStyle />
           <Helmet>
-            <title>Shade Pro</title>
+            <title>Rewood Furniture</title>
             <link rel="icon" type="image/png" href={imgFavicon} />
           </Helmet>
           <Loader id="loading" className={visibleLoader ? "" : "inActive"} />

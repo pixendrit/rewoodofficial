0e68122856c6d8131c4406c3285b7b4f59a92bca
pixendrit <pixendrit@outlook.com>
2021-04-01 10:11:45 +0200

    renditja

--- src/sections/landing5/Categories.js
+++ src/sections/landing5/Categories.js
@@ -27,19 +27,6 @@ const Categories = () => {
                 <div className="single-category w-100 rounded-10 bg-gray-2 d-inline-block px-12 pt-9 pb-12 mb-9 gr-hover-shadow-6 ">
                   <div className="case-content mb-9">
                     <h3 className="category-title gr-text-6 mb-3 text-blackish-blue">
-                      Dhoma të ndejës
-                    </h3>
-                  </div>
-                  <div className="category-img text-center">
-                    <img src={imgC1} alt="" className="" />
-                  </div>
-                </div>
-              </Link>
-
-              <Link to="/#">
-                <div className="single-category w-100 rounded-10 bg-gray-2 d-inline-block px-12 pt-9 pb-12 mb-9 gr-hover-shadow-6 ">
-                  <div className="case-content mb-9">
-                    <h3 className="category-title gr-text-6 mb-3 text-blackish-blue">
                       Kuzhina
                     </h3>
                   </div>
@@ -48,14 +35,6 @@ const Categories = () => {
                   </div>
                 </div>
               </Link>
-            </Col>
-            <Col
-              md="6"
-              lg="4"
-              data-aos="fade-up"
-              data-aos-duration="1200"
-              data-aos-delay="750"
-            >
               <Link to="/#">
                 <div className="single-category w-100 rounded-10 bg-gray-2 d-inline-block px-12 pt-9 pb-12 mb-9 gr-hover-shadow-6 ">
                   <div className="case-content mb-9">
@@ -68,6 +47,63 @@ const Categories = () => {
                   </div>
                 </div>
               </Link>
+              
+
+              
+            </Col>
+            <Col
+              md="6"
+              lg="4"
+              data-aos="fade-left"
+              data-aos-duration="1200"
+              data-aos-delay="750"
+            >
+                 <Link to="/#">
+                <div className="single-category w-100 rounded-10 bg-gray-2 d-inline-block px-12 pt-9 pb-12 mb-9 gr-hover-shadow-6 ">
+                  <div className="case-content mb-9">
+                    <h3 className="category-title gr-text-6 mb-3 text-blackish-blue">
+                      Tavolina
+                    </h3>
+                  </div>
+                  <div className="category-img text-center">
+                    <img src={imgC6} alt="" className="" />
+                  </div>
+                </div>
+              </Link>
+              <Link to="/#">
+                <div className="single-category w-100 rounded-10 bg-gray-2 d-inline-block px-12 pt-9 pb-12 mb-9 gr-hover-shadow-6 ">
+                  <div className="case-content mb-9">
+                    <h3 className="category-title gr-text-6 mb-3 text-blackish-blue">
+                      Dhoma Gjumi
+                    </h3>
+                  </div>
+                  <div className="category-img text-center">
+                    <img src={imgC3} alt="" className="" />
+                  </div>
+                </div>
+              </Link>
+           
+            </Col>
+            <Col
+              md="6"
+              lg="4"
+              data-aos="fade-up"
+              data-aos-duration="1200"
+              data-aos-delay="750"
+            >
+              <Link to="/#">
+                <div className="single-category w-100 rounded-10 bg-gray-2 d-inline-block px-12 pt-9 pb-12 mb-9 gr-hover-shadow-6 ">
+                  <div className="case-content mb-9">
+                    <h3 className="category-title gr-text-6 mb-3 text-blackish-blue">
+                      Dhoma të ndejës
+                    </h3>
+                  </div>
+                  <div className="category-img text-center">
+                    <img src={imgC1} alt="" className="" />
+                  </div>
+                </div>
+              </Link>
+              
               <Link to="/#">
                 <div className="single-category w-100 rounded-10 bg-gray-2 d-inline-block px-12 pt-9 pb-12 mb-9 gr-hover-shadow-6 ">
                   <div className="case-content mb-9">
@@ -81,38 +117,7 @@ const Categories = () => {
                 </div>
               </Link>
             </Col>
-            <Col
-              md="6"
-              lg="4"
-              data-aos="fade-left"
-              data-aos-duration="1200"
-              data-aos-delay="750"
-            >
-              <Link to="/#">
-                <div className="single-category w-100 rounded-10 bg-gray-2 d-inline-block px-12 pt-9 pb-12 mb-9 gr-hover-shadow-6 ">
-                  <div className="case-content mb-9">
-                    <h3 className="category-title gr-text-6 mb-3 text-blackish-blue">
-                      Dhoma Gjumi
-                    </h3>
-                  </div>
-                  <div className="category-img text-center">
-                    <img src={imgC3} alt="" className="" />
-                  </div>
-                </div>
-              </Link>
-              <Link to="/#">
-                <div className="single-category w-100 rounded-10 bg-gray-2 d-inline-block px-12 pt-9 pb-12 mb-9 gr-hover-shadow-6 ">
-                  <div className="case-content mb-9">
-                    <h3 className="category-title gr-text-6 mb-3 text-blackish-blue">
-                      Tavolina
-                    </h3>
-                  </div>
-                  <div className="category-img text-center">
-                    <img src={imgC6} alt="" className="" />
-                  </div>
-                </div>
-              </Link>
-            </Col>
+         
           </Row>
         </Container>
       </div>
